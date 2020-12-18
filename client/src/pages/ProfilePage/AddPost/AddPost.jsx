2fb429e35660cd058d78c890caeee08b0853f4a1
klimovcode <klimovcode@mail.ru>
2020-12-18 17:03:37 +0500

    layout addpost

--- client/src/pages/ProfilePage/AddPost/AddPost.jsx
+++ client/src/pages/ProfilePage/AddPost/AddPost.jsx
@@ -4,9 +4,9 @@ export default class AddPost extends Component {
   render() {
     return (
       <div className="addpost">
-        <h3>AddPost</h3>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button>Add post</button>
+        <h3 className="addpost__title">AddPost</h3>
+        <textarea className="addpost__text" id="" cols="30" rows="10"></textarea>
+        <button className="addpost__btn">Add post</button>
       </div>
     )
   }
