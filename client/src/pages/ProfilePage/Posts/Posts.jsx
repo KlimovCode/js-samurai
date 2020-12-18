c09b3e8bf822465f357c57a755686d2580a0b89e
klimovcode <klimovcode@mail.ru>
2020-12-18 06:10:38 +0500

    layout posts

--- client/src/pages/ProfilePage/Posts/Posts.jsx
+++ client/src/pages/ProfilePage/Posts/Posts.jsx
@@ -3,16 +3,25 @@ import avatar from '../../../assets/avatar.jpeg'
 
 export default class Posts extends Component {
   posts = [
-    { id: 1, name: 'AAA' }
+    { id: 1, author: 'AAA', text: 'abc text', likes: 1 },
+    { id: 2, author: 'BBB', text: 'bac text', likes: 3 }
   ]
+
+  getPosts = () => {
+    return this.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <Post author={post.author} text={post.text} likes={post.likes} />
+        </div>
+      )
+    })
+  }
 
   render() {
     return (
       <div className="posts">
         <h3 className="posts__title">Posts</h3>
-        <Post text="abc" />
-        <Post text="abc" />
-        <Post text="abc" />
+        {this.getPosts()}
       </div>
     )
   }
@@ -23,7 +32,12 @@ class Post extends Component {
     return (
       <div className="post">
         <img src={avatar} alt="" className="avatar" />
-        <span className="text">{this.props.text}</span>
+        <div className="post__content">
+          <span className="author"> {this.props.author} </span>
+          <span className="text"> {this.props.text} </span>
+          <span className="likes"> {this.props.likes} </span>
+        </div>
+
       </div>
     )
   }
